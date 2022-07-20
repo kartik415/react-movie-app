80e8301d8423af45490d38582d7fd80e45580d93
Kartik Rai <kartik41500@gmail.com>
2022-07-21 01:26:41 +0530

    feat: implemented tmdb api

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useState, useEffect } from 'react'
+
 import './App.css'
+import Movie from './components/Movies';
+
+const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?api_key=a04e1efd5efc6859b6f6f38348ad0164&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=";
+
+
+const SEARCH_API = "https://api.themoviedb.org/3/search/movie?api_key=a04e1efd5efc6859b6f6f38348ad0164&query=";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setsearchTerm] = useState([]);
+
+  useEffect(()=> {
+    fetch(FEATURED_API)
+      .then((res)=>res.json())
+      .then((data)=>{
+        console.log(data);
+        setMovies(data.results);
+    });
+  }, []);
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    fetch(SEARCH_API+searchTerm)
+      .then((res)=>res.json())
+      .then((data)=>{
+        setMovies(data.results);
+  });
+};
+
+  const handleOnChange = (e) => {
+    setsearchTerm(e.target.value);
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Hello there!</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+    <><header>
+      <form onSubmit={handleOnSubmit}>
+        <input type="search"  
+          onChange={handleOnChange} 
+          value={searchTerm} 
+          className='search' 
+          placeholder='Search....' 
+          />
 
-export default App
+      </form>
+      
+    </header><div className='movie-container'>
+
+        {movies.length > 0 &&
+          movies.map((movie) => <Movie key={movie.id} {...movie} />
+          )}
+      </div></>
+  )
+};
+
+export default App;
